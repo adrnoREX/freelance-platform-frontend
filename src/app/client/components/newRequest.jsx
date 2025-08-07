d85e3173b742002b1e1a6764a1f1dce0af4c8cd6
adrnoREX <128822655+adrnoREX@users.noreply.github.com>
2025-08-07 13:34:43 +0530

    Update API URL in newRequest for production

--- src/app/client/components/newRequest.jsx
+++ src/app/client/components/newRequest.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
