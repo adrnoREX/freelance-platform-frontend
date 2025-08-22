861c3ed6641b0b25d87cb67bbd435c5eabb1321b
Rupam Sen <128822655+adrnoREX@users.noreply.github.com>
2025-08-23 01:25:45 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,12 @@
 import Image from "next/image";
 import HomeComponent from "./Components/HomeComponent";
-// import Navbar from "./Components/navbar";
 import { Toaster } from "react-hot-toast";
-// import AuthModals from "./Components/AuthModals";
 
 export default function Home() {
   return (
     <div className="">
-      {/* <Navbar/> */}
       <HomeComponent/>
       <Toaster position="top-center" />
-      {/* <AuthModals/> */}
-      
     </div>
   );
 }
